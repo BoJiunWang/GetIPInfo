b7b4827340c1f4c9d4e3c89be3a4b5afebf8810e
IvanWang <bobo8347@gmail.com>
2024-03-24 13:01:43 +0800

    調整 html 畫面

--- getIP.ps1
+++ getIP.ps1
@@ -30,17 +30,23 @@ while (-not $Success -and $Retry -lt $RetryCount) {
 if ($Success) {
     $ScriptPath = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
     $OutputFile = "${ScriptPath}\ip.txt"
-    
+
+    # 檢查有沒有之前的 IP 紀錄檔
     if (Test-Path $OutputFile) {
         $ExistingContent = Get-Content $OutputFile
+        # 存在且相同就直接結束
         if ($Content -eq $ExistingContent) {
-            Write-Host "Content has not changed."
+            Write-Host "IP has not changed."
             exit
-        } else {
-            Write-Host "Content has changed. Updating file..."
+        }
+        # 如果不相同將 IP 覆寫掉
+        else {
+            Write-Host "IP has changed. Updating file..."
             Set-Content -Path $OutputFile -Value $Content
         }
-    } else {
+    }
+    # 如果沒有直接產生檔案及將 IP 寫進去
+    else {
         Write-Host "File does not exist. Creating file..."
         Set-Content -Path $OutputFile -Value $Content
     }
@@ -52,7 +58,8 @@ if ($Success) {
     # 設定 收件人和信件內容
     $Recipient = "example@gmail.com"
     $Subject = "IP has been modified!"
-    $Body = "<html lang=\'zh-Hant-TW\'><head><meta charset=\'UTF-8\'><link rel=\'stylesheet\' href=\'https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap\'></head><style>body {vertical-align: center;font-family: \'Noto Sans TC\', \'Helvetica Neue\', Helvetica, Arial, sans-serif;font-size: 20px;}</style><body><span>&#10071;&#10071;&#10071; IP has been change to: {ip}</span></body></html>".Replace("{ip}", $Response.Content)
+    $Body = "<html lang='zh-Hant-TW'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1'><link href='https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500&display=swap' rel='stylesheet'><style>body {font-family: 'Noto Sans TC', sans-serif;padding: 20px;}.alert {position: relative;color: #055160;background-color: #cff4fc;border-color: #9eeaf9;padding: 1rem 1rem;margin-bottom: 1rem;border: 1px solid transparent;border-radius: .375rem;}.alert-heading {color: inherit;}.text-danger {color: rgba(220, 53, 69, 1) !important;}.fw-bold {font-weight: bold;}h4, .h4{margin-top: 0;margin-bottom: 0.5rem;font-weight: 500;line-height: 1.2;color: inherit;font-size: calc(1.275rem + 0.3vw);}hr {margin: 1rem 0;color: inherit;border: 0;border-top: 1px solid;opacity: 0.25;}</style></head><body><div class='alert' role='alert'><h4 class='alert-heading'>IP 通知</h4><hr><div>您的 IP 地址已經更改為：<span class='text-danger fw-bold'>{ip}</span></div></div></body></html>".Replace("{ip}", $Response.Content)
+    $HTMLView = [System.Net.Mail.AlternateView]::CreateAlternateViewFromString($Body, [System.Text.Encoding]::UTF8, "text/html")
 
     # 設定 SMTP 伺服器和Port
     $SmtpServer = "smtp.gmail.com"
@@ -63,9 +70,8 @@ if ($Success) {
     $MailMessage.From = $Username
     $MailMessage.To.Add($Recipient)
     $MailMessage.Subject = $Subject
-    $MailMessage.Body = $Body
-    $MailMessage.IsBodyHtml = $true
-
+    $MailMessage.AlternateViews.Add($HTMLView)
+    
     # 建立 SMTP 客戶端
     $SmtpClient = New-Object System.Net.Mail.SmtpClient($SmtpServer, $SmtpPort)
     $SmtpClient.EnableSsl = $true
@@ -74,9 +80,13 @@ if ($Success) {
     # 發送郵件
     try {
         $SmtpClient.Send($MailMessage)
-        Write-Host "Email sent successfully."  # 郵件發送成功提示
+        # 郵件發送成功提示
+        Write-Host "Email sent successfully."
     }
     catch {
-        Write-Host "Failed to send email. $_.Exception.Message"  # 郵件發送失敗提示
+        # 郵件發送失敗提示
+        Write-Host "Failed to send email. $_.Exception.Message"
     }
 }
+
+exit
